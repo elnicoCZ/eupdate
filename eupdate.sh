e704a340f0330a157edfa27d8619cba59b358b35
Jan Kubiznak <kubiznak.jan@elnico.cz>
2015-03-14 15:49:30 +0100

    Tar works, version handling through manifest.

--- eupdate.sh
+++ eupdate.sh
@@ -11,61 +11,128 @@
 #  @version    1.0 2015-03-08: Jan Kubiznak <kubiznak.jan@elnico.cz>
 #                              Initial revision.
 #
-###############################################################################
+################################################################################
 
 #!/bin/sh
 
-###############################################################################
-# Script init and start tests
+################################################################################
+# Init
 
 clear;
 
 # Destination folder
 workdir=$(pwd)
-source=$workdir/test/eupdate/
+tgz="$1"
+
+tmp=$workdir/tmp/
+config=$workdir/config/
+source=$tmp/eupdate/
 dest=$workdir/test/root/
 
-version_old=18
-version_new=19
+manifest=manifest
+uninstall=$workdir/uninstall
+
+backup=bak
+
+################################################################################
+# Functions
+
+function quit {
+  # Performs clenaup and quits
+  if test -d $tmp; then
+    rm -r $tmp/*
+  fi
+  exit
+}
+
+################################################################################
+# Tar extract
+
+if test -e $tmp/*; then                                                         # Remove all possible content of the temp directory
+  rm -r $tmp/*
+fi
+
+if ! test -d $tmp; then                                                         # Create a temp directory if not exist
+  mkdir $tmp
+fi
+
+tar -xzf $tgz -C $tmp                                                           # Extract update to tmp
+
+################################################################################
+# Version check
+
+. $config/$manifest                                                             # Reads the installed version from the manifest file
+version_installed=$EUPDATE_VERSION
+
+. $source/$manifest                                                             # Reads the update version from the update manifest file
+version_new=$EUPDATE_VERSION
+updatable_version=$EUPDATE_APPLICABLE_FOR
+
+updatable=0                                                                     # Searches if update could be applied to the currently installed version
+for version in $updatable_version; do
+  if [ "$version" == "$version_installed" ]; then
+    updatable=1
+    break
+  fi
+done
+
+if [ "$updatable" -eq 0 ]; then                                                 # Exit if update not possible
+  echo "Update from version $version_installed to version $version_new not possible."
+  quit
+fi
+
+echo "Updating from version $version_installed to version $version_new"
+cp $config/$manifest $config/$manifest.prev                                     # Overwrites the manifest file
+cp $source/$manifest $config/$manifest
+
+echo "rm $uninstall" > $uninstall
+if test -f $config/$manifest.prev; then
+  sed -i "1irm $config/$manifest.prev" $uninstall
+  sed -i "1icp $config/$manifest.prev $config/$manifest" $uninstall
+fi
+
+chmod +x $uninstall
+
+################################################################################
+# ...
 
 cd $source
 tree="$(find)"
 
-echo -e "Updating from $version_old to $version_new"
-
 # Obtains a list of all directories in source.
 for file in $tree; do
-  if test -d $source/$file
-  then
-    #echo "$file"
+  if test -d $source/$file; then
     dirs="$dirs $file"
   fi
 done
 
 # Obtains a list of all files in source.
 for file in $tree; do
-  if test -f $source/$file
-  then
-    #echo -e "$file"
+  if test -f $source/$file; then
     files="$files $file"
   fi
 done
 
+
+
 # Creates a backup file for every file in dest which is to be overwritten.
 # The backup filename is: "filename.version"
 for file in $files; do
-  if test -f $dest/$file
-  then
+  if test -f $dest/$file; then
     echo -e "Making a backup for $dest/$file"
-    cp $dest/$file $dest/$file.$version_old
+    
+    sed -i "1irm $dest/${file}.${backup}" $uninstall
+    sed -i "1icp $dest/${file}.${backup} ${dest}/${file}" $uninstall
+    cp $dest/$file $dest/$file.$backup
   fi
 done
 
 # Creates directories which exist in source but not in dest.
 for file in $dirs; do
-  if ! test -d $dest/$file
-  then
+  if ! test -d $dest/$file; then
     echo -e "Creating new directory $dest/$file"
+    
+    sed -i "1irmdir $dest/$file" $uninstall
     mkdir $dest/$file
   fi
 done
@@ -73,5 +140,14 @@ done
 # Copies all files from source to dest.
 for file in $files; do
   echo -e "Copying $dest/$file"
+  
+  sed -i "1irm $dest/$file" $uninstall
   cp $source/$file $dest/$file
 done
+
+sed -i "1i#!/bin/sh" $uninstall
+
+################################################################################
+# Cleanup and quit
+
+quit
